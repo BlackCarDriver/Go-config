c2ff17ee1862ab11701e344112b057d82da46b2c
BlackCarDriver <13169346002@163.com>
2019-06-14 23:27:22 +0800

    writing version 2.0

--- config/config.go
+++ config/config.go
@@ -10,34 +10,48 @@ import(
 	"errors"
 	"strings"
 	"bufio"
-	"bytes"
 )
 /*
 explain of Config struct:
-	configPath is the root path of config files, all files with .conf suffix will be read into rawConf
+configPath is the root path of config files, all files with .conf suffix will be read into rawConf
 when the struct is init. 
-	rawConf is the map tmpely saving the string that read from config files, those string will not be
+rawConf is the map tmpely saving the string that read from config files, those string will not be
 used until you register then.
-	ripeConf is the map saving config value, those config is read from rawConf through Register()
+ripeConf is the map saving config value, those config is read from rawConf through Register()
 */
+
+var readHistory = make(map[string]bool)
+
 type Config struct{
 	configPath string
-	rawConf map[string]string;
+	rawConf map[string]string 
 	ripeConf map[string]interface{}
 }
 
 type ConfigMachine interface {
-	readAllConfig()	error
 	InitWithFilesPath(filesPath string) error
 	Register(keyName string , dfValue interface{}, isImportant bool)(err error, warn string)
 	Get(keyName string) (value interface{}, err error)
-	DisplyConfList()
 }
 
+func handleErr(prefix string ,err error, isSeriou bool) ( errNotNull bool) {
+	if err == nil {
+		return false
+	}
+	fmt.Println(prefix , err)
+	if isSeriou {
+		os.Exit(2)
+	}
+	return true	 
+}
+
+//=========== method in interface ===============
 func (c *Config) InitWithFilesPath(Configpath string) error{
 	if c.configPath != "" {
 		return errors.New("You can't init the Confi twice!")
 	}
+	c.rawConf = make(map[string]string)
+	c.ripeConf = make(map[string]interface{}) 
 	c.configPath = Configpath;
 	c.readAllConfig()
 	return nil
@@ -55,17 +69,6 @@ func (c *Config) Get(keyName string)(value interface{}, err error){
 	}else{
 		err = fmt.Errorf("KeyName %v not found in config list!", keyName)
 		return 
-	}
-}
-
-func (c *Config) DisplyConfList(){
-	fmt.Println("============= rawConf ========")
-	for k,v := range c.rawConf {
-		fmt.Println(k, " ---> ",v)
-	}
-	fmt.Println("============ ripefMap ========")
-	for k,v := range c.ripeConf {
-		fmt.Println(k, " ---> ",v)
 	}
 }
 
@@ -100,39 +103,93 @@ func (c *Config) Register(confName string, dfValue interface{}, isStrict bool)(e
 	return
 }
 
-func (c *Config)readAllConfig() error {
-	file,err := os.Open(c.configPath)
-	if err!=nil {
+//==============================================
+
+func (c *Config) DisplyConfList(){
+	fmt.Println("============= rawConf ========")
+	for k,v := range c.rawConf {
+		fmt.Println(k, " ---> ",v)
+	}
+	fmt.Println("============ ripefMap ========")
+	for k,v := range c.ripeConf {
+		fmt.Println(k, " ---> ",v)
+	}
+}
+
+//read all files with .conf suffix in configPath
+func (c *Config)readAllConfig()( haveError bool ) {
+	filesPath := c.configPath
+	file ,err := os.Open( filesPath )
+	handleErr("os.Open(filesPaht) ",err, true)
+	defer file.Close()
+	fi, err := file.Readdir(0)
+	handleErr("file.Readdir(0) ",err, true)
+	errCounter := 0
+	for _, info := range fi {
+		//guarante each file only read one times
+		if readHistory[info.Name()] {
+			fmt.Println(info.Name(), " already read before ...")
+			continue;
+		}
+		readHistory[info.Name()] = true
+		tmpPath := filesPath + info.Name()
+		err := c.readConfig(tmpPath)
+		if handleErr("c.readConfig(tmpPath) ",err,false) {
+			errCounter ++
+		}
+	}
+	return errCounter==0
+}
+
+//read a config file and save message into Conf.rawMap
+func (c *Config)readConfig(path string) error {
+	file,err := os.Open(path)
+	if handleErr("os.Open(path) ", err, false) {
 		return err
 	}
 	defer file.Close()
 	buf := bufio.NewReader(file)
 	for{
-		line, _, err := buf.ReadLine()
-		if err == io.EOF {
+		lineByte, _, err := buf.ReadLine()
+		line := strings.TrimSpace( string(lineByte) )
+		if err == io.EOF {	//end of file
 			break
 		}
-		if err != nil {
-			return nil
+		if err != nil {		//other error
+			fmt.Println(err)
+			return err
 		}
-		if bytes.Equal(line,  []byte{}) {
+		if line == "" {		//ignore empty line
 			continue
 		}
-		if bytes.HasPrefix(line, []byte{'#'} ) {
+		if strings.HasPrefix(line, "#") {	//ignore cmment
 			continue
 		}
-		tmpStr := strings.TrimSpace(string(line))
-		index := strings.Index(tmpStr, "=")
-		if index <= 0 {
-			continue
+		index := strings.Index(line, "=")
+		if index <= 0 {						//unknow format
+			return errors.New("Unknow format in config files when reading : " + line)
 		}
-		key := strings.TrimSpace(tmpStr[:index])
-		value := strings.TrimSpace(tmpStr[index+1:])
-		if len(key) == 0 || len(value) == 0 {
-			continue
+		confName := strings.TrimSpace(line[:index])
+		confValue := strings.TrimSpace(line[index+1:])
+		if len(confName) == 0 || len(confValue) == 0 {	//unknow format
+			return errors.New("Unknow format in config files when reading : " + line)
 		}
-		c.rawConf[key] = value
+		//============need to do somtthing
+	
+		fmt.Println(confName, " ----> ", confValue)
+
+		//=================================	
 	}
 	return nil
 }
 
+//judge if a name of config is legal
+func isLegalName(name string) bool {
+	//need to do something...
+	return true
+}
+
+func Test(){
+	var tc Config
+	tc.InitWithFilesPath("./config/conf/")
+}
--- main.go
+++ main.go
@@ -2,34 +2,14 @@ package main
 
 import(
 	"./config"
-	"fmt"
 )
 
 func main(){
-	testInit()
-	testUseage()
+	config.Test()
 }
 
 func testInit(){
-	//create an Config object by config filename
-	conf, _ := config.NewConfig("demo1.conf")
-	//register some config valua
-	conf.RegisterConf("myname","haha",true)
-	conf.RegisterConf("isman",false, true)
-	//create an new config value taht dont exist in config file
-	conf.RegisterConf("newconf","testnewconf",false)
 }
 
 func testUseage(){
-	//demo of create more than one Config object
-	config.NewConfig("demo2.conf")
-	//use GetConfig() to get an Config object
-	demo1 := config.GetConfig("demo2.conf")
-	demo1.RegisterConf("host","123.123.123.123",true)
-	//display the value that in the map
-	demo1.DisplayConf()
-	demo2 := config.GetConfig("demo1.conf")
-	name := demo2.Get("myname")
-	isman := demo2.Get("isman")
-	fmt.Println(name,"   ",isman)
 }
